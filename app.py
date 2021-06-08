037914155edbafff175bcb105b0b9bc8c51b0ed1
rom6621 <3103rom@gmail.com>
2021-06-08 15:49:26 +0900

    fix

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from logging import debug
 from flask import Flask, render_template
 from local_settings import cliend_id
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.debug = True
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -24,6 +26,10 @@ def displayThread():
 def displayThreadList():
     return render_template('threadList.html')
 
+@socketio.on('checkToken')
+def checkToken(token):
+    print(token)
+
 # 実行
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, host='0.0.0.0' ,port=5000)
