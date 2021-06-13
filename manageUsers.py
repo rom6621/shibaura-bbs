9b73b1d203692caccec5707081e3733a5e7ff32b
Fumin66 <al19066@shibaura-it.ac.jp>
2021-06-13 21:32:59 +0900

    fix

--- manageUsers.py
+++ manageUsers.py
@@ -13,32 +13,35 @@
 ###            なければ、登録してUseIDとMailAddressを返す。
 ### Return : UserID,MailAddress
 #################################################
+import sqlite3
+import class
 
-def User_Registration_Request(MailAddress):
+def User_Registration_Request(mailAddress):
     dbname = 'BBS.db' #データベース作成　or 参照
     conn = sqlite3.connect(dbname)
     # sqliteを操作するカーソルオブジェクトを作成
     cur = conn.cursor()
 
-    UserID = ''
+    id = ''
     #DBの中のtableの中身を確認
-    cur.execute('SELECT MailAddress FROM User WHERE MailAddress = '+MailAddress)
+    cur.execute('SELECT mailAddress FROM User WHERE mailAddress = '+mailAddress)
     
     #DB内になかった場合ユーザIDにNullを代入し、メールアドレスを返す
     if cur.fetchall() ==None :
         #ユーザID作成処理
-        UserID = MailAddress.split("@")[0]
+        id = MailAddress.split("@")[0]
 
         #tableへの登録
-        sql = 'INSERT INTO User (UserID, MailAddress) values ('+UserID+','+MailAddress+')'
+        sql = 'INSERT INTO User (Userid, MailAddress) values ('+id+','+mailAddress+')'
         cur.execute(sql)
 
 
     #DB内にあったとき
     else:
         row = cur.fetchall()
-        #tableがまだ決まっていないのでrow[1](UseID),row[2](Address)
-        UserID = row[1] 
-        MailAddress = row[2]
+        #tableがまだ決まっていないのでrow[0](UseID)
+        id = row[0] 
 
-    return UserID,MailAddress
+    user = class.User(id,mailAddress)
+    con.close()
+    return user
