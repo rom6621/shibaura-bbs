3dc209f24c2043ebbb6077f00d42d5c355d261d8
rom6621 <3103rom@gmail.com>
2021-06-13 18:52:23 +0900

    Update class.py

--- class.py
+++ class.py
@@ -3,10 +3,10 @@ class Entry:
     auther: str
     content: str
 
-    def __init__(self, entry_id, entry_auther, entry_content):
-        self.id = entry_id
-        self.auther = entry_auther
-        self.content = entry_content
+    def __init__(self, entryId, entryAuther, entryContent):
+        self.id = entryId
+        self.auther = entryAuther
+        self.content = entryContent
 
     def deleteEntry(self):
         self.content = "削除されました"
@@ -15,17 +15,17 @@ class Thread:
     id: int
     name: str
     details: str
-    last_entry_id: int
+    lastEntryId: int
 
-    def __init__(self, thread_id, thread_name, thread_details):
-        self.id = thread_id
-        self.name = thread_name
-        self.details = thread_details
-        self.last_entry_id = 0
+    def __init__(self, threadId, threadName, threadDetails):
+        self.id = threadId
+        self.name = threadName
+        self.details = threadDetails
+        self.lastEntryId = 0
         self.entries = []
 
-    def addEntry(self, entry_auther, entry_content):
-        self.last_entry_id += 1
-        new = Entry(self.last_entry_id, entry_auther, entry_content)
+    def addEntry(self, entryAuther, entryContent):
+        self.lastEntryId += 1
+        new = Entry(self.lastEntryId, entryAuther, entryContent)
         self.entries.append(new)
 
