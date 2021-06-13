e3dc3873b0c70b1a9fed1b493d0e9aa1978e1cee
rom6621 <3103rom@gmail.com>
2021-06-13 18:40:38 +0900

    Update class.py

--- class.py
+++ class.py
@@ -1,10 +1,22 @@
 class Entry:
+    id: int
+    auther: str
+    content: str
+
     def __init__(self, entry_id, entry_auther, entry_content):
         self.id = entry_id
         self.auther = entry_auther
         self.content = entry_content
 
+    def deleteEntry(self):
+        self.content = "削除されました"
+
 class Thread:
+    id: int
+    name: str
+    details: str
+    last_entry_id: int
+
     def __init__(self, thread_id, thread_name, thread_details):
         self.id = thread_id
         self.name = thread_name
