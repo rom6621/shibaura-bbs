66b439a688779a3d9b19031cf7dcb420b9947a21
Fumin66 <al19066@shibaura-it.ac.jp>
2021-06-15 15:47:41 +0900

    Update manageUsers.py

--- manageUsers.py
+++ manageUsers.py
@@ -24,21 +24,20 @@ def User_Registration_Request(mailAddress):
 
     id = ''
     #DBの中のtableの中身を確認
-    cur.execute('SELECT mailAddress FROM User WHERE mailAddress =  '+ mailAddress)
-    
+    cur.execute('SELECT mailAddress FROM User WHERE mailAddress = " '+ mailAddress+'"')
+    row = cur.fetchall()
     #DB内になかった場合ユーザIDにNullを代入し、メールアドレスを返す
-    if cur.fetchall() ==None :
+    if len(row) == 0 :
         #ユーザID作成処理
         id = mailAddress.split("@")[0]
 
         #tableへの登録
-        sql = 'INSERT INTO User (userId, mailAddress) values (' + id + ',' + mailAddress + ')'
+        sql = 'INSERT INTO User (id, mailAddress) values ("' + id + '", "' + mailAddress + '" )'
         cur.execute(sql)
 
 
     #DB内にあったとき
     else:
-        row = cur.fetchall()
         #tableがまだ決まっていないのでrow[0](UseID)
         id = row[0]
 
