abe2701e9cfc94d93f7974ec3f05c73ccb758b41
al19026 <al19026@shibaura-it.ac.jp>
2021-06-15 16:44:46 +0900

    Update manageEntry.py

--- manageEntry.py
+++ manageEntry.py
@@ -11,3 +11,37 @@
 ### Function: 
 ### Return : 
 #################################################
+import sqlite3
+
+#書込情報問い合わせ
+#user_idとcontents_idが一致する書込がデータベースにあると0を返し、なかったら1を返す
+def Contents_Processing(userID,contentsID):
+    conn = sqlite3.connect('DBS.db')
+    c = conn.cursor()
+    if(c.execute('SELECT CONTENTS FROM BBSManage WHERE USER_ID = "'+userID +'" AND CONTENTS_ID = '+str(contentsID))!=None):
+        #書込があったとき
+        conn.close()
+        return 0
+    else:
+        #書込がなかったとき
+        conn.close()
+        return 1
+
+
+#書込削除
+#user_idとcontents_idが一致する書込を更新する
+def Delete_Contents(userID,contentsID):
+    conn = sqlite3.connect('DBS.db')
+    c = conn.cursor()
+    data=('書込を削除しました.')
+    c.execute('UPDATE BBSManage SET CONTENTS = "'+data+'" WHERE USER_ID = "'+userID+'" AND CONTENTS_ID = '+str(contentsID))
+    conn.close()
+
+
+#書込追加
+#書込を追加する
+def Add_Contents(userID,contentsID,contents):
+    conn = sqlite3.connect('DBS.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO BBSManage VALUES("'+userID+'", ' +str(contentsID)+', "' +contents+ '")')
+    conn.close()
