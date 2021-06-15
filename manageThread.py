a44f0b181013639d398c9b29351ef54c0df563da
Ishikawa721 <al19128@shibaura-it.ac.jp>
2021-06-15 16:32:01 +0900

    Update manageThread.py

--- manageThread.py
+++ manageThread.py
@@ -1,13 +1,36 @@
 #################################################
-### Designer : 
-### Date : 
-### Purpose : 
+# Designer :
+# Date :
+# Purpose :
 #################################################
 
 #################################################
-### Function Name : 
-### Designer : 
-### Date : 
-### Function: 
-### Return : 
+# Function Name :
+# Designer :
+# Date :
+# Function:
+# Return :
 #################################################
+
+import sqlite3
+
+
+def Thread_Registration(threadName, tagName):
+
+    conn = sqlite3.connect("test.db")
+    cur = conn.cursor()
+
+    sql = 'INSERT INTO thread (threadName, tagName) values ("' + \
+        threadName + '","' + tagName + '")'
+    cur.execute(sql)
+
+    sql = 'SELECT threadID FROM thread ORDER BY threadID DESC LIMIT 1'
+
+    cur.execute(sql)
+
+    threadID = cur.fetchall[0]
+
+    cur.close()
+    conn.close()
+
+    return threadID
