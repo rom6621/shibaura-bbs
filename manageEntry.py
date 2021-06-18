48ec25ef5bffbf92d08c7ba86221685491251c83
al19026 <al19026@shibaura-it.ac.jp>
2021-06-18 18:09:11 +0900

    fix

--- manageEntry.py
+++ manageEntry.py
@@ -1,24 +1,25 @@
 #################################################
-### Designer : 
-### Date : 
-### Purpose : 
+### Designer : 保科貴大
+### Date : 2021.6.18
+### Purpose : 書込情報管理部
 #################################################
 
 #################################################
-### Function Name : 
-### Designer : 
-### Date : 
-### Function: 
-### Return : 
+### Function Name : Contents_Processing
+### Designer : 保科貴大
+### Date :  2021.6.18
+### Function: userIDとcontentIDからcontentを抜き出しcontentが
+###　　　　　　があるなら0,ないなら1を返す
+### Return : 0または1
 #################################################
 import sqlite3
 
 #書込情報問い合わせ
 #user_idとcontents_idが一致する書込がデータベースにあると0を返し、なかったら1を返す
-def Contents_Processing(userID,contentsID):
-    conn = sqlite3.connect('DBS.db')
+def Contents_Processing(userID,contentID):
+    conn = sqlite3.connect('test.db')
     c = conn.cursor()
-    if(c.execute('SELECT CONTENTS FROM BBSManage WHERE USER_ID = "'+userID +'" AND CONTENTS_ID = '+str(contentsID))!=None):
+    if(c.execute('SELECT content FROM Entry WHERE auther = "'+userID +'" AND id = '+str(contentID)+'')!=None):
         #書込があったとき
         conn.close()
         return 0
@@ -30,18 +31,18 @@ def Contents_Processing(userID,contentsID):
 
 #書込削除
 #user_idとcontents_idが一致する書込を更新する
-def Delete_Contents(userID,contentsID):
-    conn = sqlite3.connect('DBS.db')
+def Delete_Contents(userID,contentID):
+    conn = sqlite3.connect('test.db')
     c = conn.cursor()
     data=('書込を削除しました.')
-    c.execute('UPDATE BBSManage SET CONTENTS = "'+data+'" WHERE USER_ID = "'+userID+'" AND CONTENTS_ID = '+str(contentsID))
+    c.execute('UPDATE Entry SET content = "'+data+'" WHERE auther = "'+userID+'" AND id = '+str(contentID)+'')
     conn.close()
 
 
 #書込追加
 #書込を追加する
-def Add_Contents(userID,contentsID,contents):
-    conn = sqlite3.connect('DBS.db')
+def Add_Contents(userID,contentID,content):
+    conn = sqlite3.connect('test.db')
     c = conn.cursor()
-    c.execute('INSERT INTO BBSManage VALUES("'+userID+'", ' +str(contentsID)+', "' +contents+ '")')
+    c.execute('INSERT INTO Entry(id,auther,content) VALUES('+str(contentID)+',  "'+userID+'", "'+content+'")')
     conn.close()
