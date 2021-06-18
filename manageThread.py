e5bdc264a19da5966d2152643c0b06dfcb378e75
Ishikawa721 <al19128@shibaura-it.ac.jp>
2021-06-18 20:56:15 +0900

    fix

--- manageThread.py
+++ manageThread.py
@@ -34,3 +34,47 @@ def Thread_Registration(threadName, tagName):
     conn.close()
 
     return threadID
+
+
+#################################################
+# Function Name :search
+# Designer :
+# Date :
+# Function:
+# Return :
+#################################################
+
+
+def search(searchKeys):
+    searchWords = searchKeys[0]
+    searchTags = searchKeys[1]
+
+    conn = sqlite3.connect("tes")
+    cur = conn.cursor()
+
+    sql = 'SELECT * FROM Thread'
+    if (len(searchWords) != 0) or (len(searchTags) != 0):
+        sql += ' WHERE'
+        if len(searchWords) != 0:
+            sql += ' name in ('
+            for searchWord in searchWords:
+                sql += ('"' + searchWord + '", ')
+            sql.pop()
+            sql += ')'
+            if len(searchTags) != 0:
+                sql += ' OR'
+        if len(searchTags) != 0:
+            sql += ' name in ('
+            for searchTag in searchTags:
+                sql += ('"' + searchTag + '", ')
+            sql.pop()
+            sql += ')'
+
+    cur.execute(sql)
+
+    searchKey = cur.fetchall[0]
+
+    cur.close()
+    conn.close()
+
+    return searchKey
