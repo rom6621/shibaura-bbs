71944b3d30e1a554979fc2a9b993afeae3778888
al19085 <al19085@shibaura-it.ac.jp>
2021-06-22 14:29:03 +0900

    Update entry.py

--- entry.py
+++ entry.py
@@ -20,6 +20,7 @@
 
 from flask import Flask, request, render_template
 import thread
+import manageEntry
 
 class Entry:
     id: int #書込の順番
@@ -35,23 +36,16 @@ class Entry:
         self.content = '削除されました'
         return self
 
-"""def d_addContents(entry, thread):
-    print(entry.id, entry.auther, entry.content, thread.name)
-
-def d_deleteContents(thread, entry):
-    print(entry.id, entry.auther, entry.content, thread.name)   テスト用コード"""
-
 #書込みを登録する関数
 def writeEntry(thread, entryAuther, entryContent):
-    new = thread.addEntry(entryAuther, entryContent)
+    newEntry = thread.addEntry(entryAuther, entryContent)
     #addEntryで作成されたnewインスタンスを用いて関数を呼び出す
-    addContents(new, thread) #本物のコード
-    """d_addContents(new, thread)
-    return new   テスト用コード"""
+    newThread = addContents(new, thread)
+    return newThread
 
 #書込みを削除する関数
 def deleteEntry(thread, entry):
     #書き込み内容のみを置き換えたものをnewインスタンスに入れる
-    new = entry.exchangeContent()
-    deleteContents(thread, new) #本物のコード
-    #d_deleteContents(thread, new)   テスト用コード
+    newEntry = entry.exchangeContent()
+    newThread = deleteContents(new, thread)
+    return newThread
